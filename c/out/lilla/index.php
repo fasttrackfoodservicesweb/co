6bfebf6e396563985e3029bf2daf2d43e15cb00a
YOUR NAME <frsd14@yahoo.com>
2016-01-17 23:18:02 +0100

    add test folders

---
+++ c/out/lilla/index.php
@@ -0,0 +1,25 @@
+ <?php
+                        $adrandroid= 'se.subitoservices.fasttrack.lillasaigon';$adrios= 'https://itunes.apple.com/us/app/lilla-saigon/id1071386433?l=sv&ls=1&mt=8';
+                       echo '
+                       <!DOCTYPE html>
+                       <html lang="en">
+                       <head>
+                       <title>Lilla Saigon</title>
+                       <style>
+                       .in {
+                               margin-left: auto;
+                                 margin-right: auto;
+                                   display: table;
+                       }
+                       body {
+                               background-color: lightgrey;
+                       }
+                       </style>
+                       </head>
+                       <body>
+                       ';
+                       echo '<br /><br /><div class="in"><a href="'.$adrios.'">
+                         <img src="../../img/app1.png" alt="App Store"></a>';
+                       echo '<br /><br /><br /><br /><a href="'.$adrandroid.'">
+                        <img src="../../img/go1.png" alt="Google Play"></a></div>';
+                       echo'</body></html>';
